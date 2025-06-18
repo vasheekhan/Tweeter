86932400211db3ef7320d6f57960bc6932a1719c
Vashee khan <vashee.khan.lits@laxmi.edu.in>
2025-06-18 14:49:50 +0530

    updated comment model in comment model added onmodel property userId property commentable property

--- src/models/comment.js
+++ src/models/comment.js
@@ -8,6 +8,21 @@ const commentSchema = new mongoose.Schema({
     userEmail: {
         type: String
     },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    }
 }, {timestamps: true});
 
 const Comment = mongoose.model('Comment', commentSchema);
